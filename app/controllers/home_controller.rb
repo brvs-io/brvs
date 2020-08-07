44a524a07d166064001f1e4f4a220bb7cb4c809b
Tony Burns <tony@tonyburns.net>
2020-08-06 21:46:35 -0400

    feat: create homepage controller

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+# Homepage controller.
+class HomeController < ApplicationController
+end
---
+++ spec/requests/home_request_spec.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'HomeController', type: :request do
+  describe 'GET /' do
+    it 'returns HTTP success' do
+      get '/'
+      expect(response).to have_http_status(:ok)
+    end
+  end
+end
