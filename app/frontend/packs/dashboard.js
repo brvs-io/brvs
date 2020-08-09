7a611b6f5f5c2c6950dbc80b8322e8beee6fa9e5
Tony Burns <tony@tonyburns.net>
2020-08-09 14:41:29 -0400

    test: add babel support for eslint

--- app/frontend/packs/dashboard.js
+++ app/frontend/packs/dashboard.js
@@ -1,3 +1,7 @@
-require("@rails/ujs").start();
-require("turbolinks").start();
-require("local-time").start();
+import Rails from "@rails/ujs";
+import Turbolinks from "turbolinks";
+import LocalTime from "local-time";
+
+Rails.start();
+Turbolinks.start();
+LocalTime.start();
